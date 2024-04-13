306a40cb39e6c908f5ffb69dc810bdc08b9bedcb
Dhruv Dhari <dhruvdhari2003@gmail.com>
2024-04-13 09:25:29 +0530

    try 6

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import styled, { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from "react-router-dom";
 import { darkTheme } from './utils/Themes';
 import Navbar from "./components/sections/Navbar"
-// import Hero from "./components/sections/Hero"
-// import Skills from "./components/sections/Skills"
-// import Education from "./components/sections/Education"
+import Hero from "./components/sections/Hero"
+import Skills from "./components/sections/Skills"
+import Education from "./components/sections/Education"
 
 
 const Body = styled.div`
@@ -16,21 +16,21 @@ const Body = styled.div`
   position: relative;
 `
 
-// const Wrapper = styled.div`
-//   padding-bottom: 100px;
-//   background: linear-gradient(
-//       38.73deg,
-//       rgba(204, 0, 187, 0.15) 0%,
-//       rgba(201, 32, 184, 0) 50%
-//     ),
-//     linear-gradient(
-//       141.27deg,
-//       rgba(0, 70, 209, 0) 50%,
-//       rgba(0, 70, 209, 0.15) 100%
-//     );
-//   width: 100%;
-//   clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
-// `;
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
   return (
@@ -38,11 +38,11 @@ function App() {
     <BrowserRouter>
     <Navbar/>
     <Body>
-      {/* <Hero/>
+      <Hero/>
       <Wrapper>
       <Skills/>
       <Education/>
-      </Wrapper> */}
+      </Wrapper>
     </Body>
     </BrowserRouter>
    </ThemeProvider>
--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,70 +1,70 @@
-// import React, { useState } from "react";
-// import styled, { useTheme } from "styled-components";
-// // import { NavLink } from "react-router-dom";
-// import { MenuRounded } from "@mui/icons-material";
-// import { Link as LinkR } from "react-router-dom";
+import React, { useState } from "react";
+import styled, { useTheme } from "styled-components";
+// import { NavLink } from "react-router-dom";
+import { MenuRounded } from "@mui/icons-material";
+import { Link as LinkR } from "react-router-dom";
 
-// const Nav = styled.div`
-//   background-color: ${({ theme }) => theme.bg};
-//   height: 80px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   font-size: 1rem;
-//   position: sticky;
-//   top: 0;
-//   z-index: 10;
-//   color: white;
-// `;
-// const ColorText = styled.div`
-//   color: ${({ theme }) => theme.primary};
-//   font-size: 32px;
-// `;
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+`;
+const ColorText = styled.div`
+  color: ${({ theme }) => theme.primary};
+  font-size: 32px;
+`;
 
-// const NavbarContainer = styled.div`
-//   width: 100%;
-//   max-width: 1200px;
-//   padding: 0 24px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   font-size: 1rem;
-// `;
-// const NavLogo = styled(LinkR)`
-//   display: flex;
-//   align-items: center;
-//   width: 80%;
-//   padding: 0 6px;
-//   font-weight: 500;
-//   font-size: 18px;
-//   text-decoration: none;
-//   color: inherit;
-// `;
+const NavbarContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+const NavLogo = styled(LinkR)`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  padding: 0 6px;
+  font-weight: 500;
+  font-size: 18px;
+  text-decoration: none;
+  color: inherit;
+`;
 
-// const NavItems = styled.ul`
-//   width: 100%;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   gap: 32px;
-//   padding: 0 6px;
-//   list-style: none;
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
 
-//   @media screen and (max-width: 768px) {
-//     display: none;
-//   }
-// `;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
-// const NavLink = styled.a`
-//   color: ${({ theme }) => theme.text_primary};
-//   font-weight: 500;
-//   cursor: pointer;
-//   transition: all 0.2s ease-in-out;
-//   text-decoration: none;
-//   &:hover {
-//     color: ${({ theme }) => theme.primary};
-//   }
-// `;
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
 
 // const ButtonContainer = styled.div`
 //   width: 80%;
@@ -97,16 +97,16 @@
 //   }
 // `;
 
-// const MobileIcon = styled.div`
-//   height: 100%;
-//   display: flex;
-//   align-items: center;
-//   color: ${({ theme }) => theme.text_primary};
-//   display: none;
-//   @media screen and (max-width: 768px) {
-//     display: block;
-//   }
-// `;
+const MobileIcon = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
 
 // const MobileMenu = styled.ul`
 //   width: 100%;
@@ -131,41 +131,41 @@
 //   opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
 //   z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
 // `;
-// const Navbar = () => {
-//     const [isOpen, setIsOpen] = useState(false);
-//   const theme = useTheme();
-//   return (
-//     <Nav>
-//       <NavbarContainer>
-//         <NavLogo to="/">
-//         <ColorText>&lt;</ColorText>Dhruv
-//           <div style={{ color: theme.primary }}>/</div>Dhari
-//           <ColorText>&gt;</ColorText>
-//         </NavLogo>
-
-//         <MobileIcon onClick={() => setIsOpen(!isOpen)}>
-//           <MenuRounded style={{ color: "inherit" }} />
-//         </MobileIcon>
-//         <NavItems>
-//         <NavLink href="#About">About</NavLink>
-//           <NavLink href="#Skills">Skills</NavLink>
-//           <NavLink href="#Experience">Experience</NavLink>
-//           <NavLink href="#Projects">Projects</NavLink>
-//           <NavLink href="#Education">Education</NavLink>
-//         </NavItems>
-//       </NavbarContainer>
-//     </Nav>
-//   );
-// };
-
-// export default Navbar;
-
-import React from 'react'
-
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  const theme = useTheme();
   return (
-    <div>Navbar</div>
-  )
-}
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">
+        <ColorText>&lt;</ColorText>Dhruv
+          <div style={{ color: theme.primary }}>/</div>Dhari
+          <ColorText>&gt;</ColorText>
+        </NavLogo>
 
-export default Navbar
+        <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+          <MenuRounded style={{ color: "inherit" }} />
+        </MobileIcon>
+        <NavItems>
+        <NavLink href="#About">About</NavLink>
+          <NavLink href="#Skills">Skills</NavLink>
+          <NavLink href="#Experience">Experience</NavLink>
+          <NavLink href="#Projects">Projects</NavLink>
+          <NavLink href="#Education">Education</NavLink>
+        </NavItems>
+      </NavbarContainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
+
+// import React from 'react'
+
+// const Navbar = () => {
+//   return (
+//     <div>Navbar</div>
+//   )
+// }
+
+// export default Navbar
